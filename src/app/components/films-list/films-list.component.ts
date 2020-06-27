8d2c05adc8c971ecfca947a99e60a484b004fb84
josebayarri <josemiguelbayarri@gmail.com>
2020-06-27 22:33:57 +0200

    maquetando los estilos del index y film-ist

--- src/app/components/films-list/films-list.component.ts
+++ src/app/components/films-list/films-list.component.ts
@@ -1,22 +1,22 @@
-import { Component, OnInit } from '@angular/core';//importación por defecto de angular
+import { Component, OnInit } from '@angular/core';//importamos la variable component y oninit que son default de angular
 import { MovieService } from '../../service/movie.service';//importación del servicio movieservice siguiendo la ruta de carpetas y archivos
 
 @Component({
   selector: 'app-films-list',
-  templateUrl: './films-list.component.html',
-  styleUrls: ['./films-list.component.scss']
+  templateUrl: './films-list.component.html',//conexion con el html de este componente
+  styleUrls: ['./films-list.component.scss']//conexion con el css de este componente
 })
 export class FilmsListComponent implements OnInit {
 
 
-  pelisMostrar: object;
+  pelisMostrar: object;//creamos el objeto pelismostrar con la intencion de meter algo dentro
 
-  constructor(public MovieService:MovieService) { }
+  constructor(public MovieService:MovieService) { }//hacemos referencia la archivo del service movieservice
 
-  ngOnInit() {
-    this.MovieService.getMovies()
+  ngOnInit() {//cuando se active gracias a oninit apliucamos la logico dentro de los siguientes parentesis
+    this.MovieService.getMovies()//hace referencia a la funcion get movies que esta programada en movieservice
     .subscribe(
-      res => this.pelisMostrar = res,
+      res => this.pelisMostrar = res,//manda como respuesta el objeto pelisMostrar lleno con lo que fluye de getMovies y pasamos pelimostrar al ngfor del html que lo divide en raciones
       error => console.error(error),
       () => console.log(this.pelisMostrar)
     )
